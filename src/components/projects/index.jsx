9bcca3fac8cdc22d7d39e01c51c58723fe7d0773
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-03-05 23:14:06 +0530

    Fix: Changes titles

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -11,27 +11,27 @@ const data = [
         speed: 0.5
     },
     {
-        title: "UFC",
+        title: "Rivian",
         description: "Developed the Future of UFC Sports Ecosystem despite not being a sports fan.",
         speed: 0.5
     },
     {
-        title: "Lincoln",
+        title: "Tesla",
         description: "Defined the visual concept and design language for the Lincoln Zephyr 2022 but never seen it in real life.",
         speed: 0.67
     },
     {
-        title: "Royal Caribbean",
+        title: "MClaren",
         description: "I was just one person on a massive team that created an entire Royal Caribbean eco-system.",
         speed: 0.8
     },
     {
-        title: "Sleepiq",
+        title: "Ferrari N.V.",
         description: "Designed a 1M+ users product utilizing my best personal experience: sleeping.",
         speed: 0.8
     },
     {
-        title: "NFL",
+        title: "BMW",
         description: "Explored the Future of Fantasy Football while being in a country where football means a total different sport.",
         speed: 0.8
     }
